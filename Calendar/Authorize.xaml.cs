14bb3a95eacf0e577f633e272174c7d1598cf906
yurasenchuk <63191681+yurasenchuk@users.noreply.github.com>
2020-10-29 23:17:20 +0200

    created authorization view

---
+++ Calendar/Authorize.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Calendar
+{
+    public partial class Authorize : Window
+    {
+        public Authorize()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorize_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
